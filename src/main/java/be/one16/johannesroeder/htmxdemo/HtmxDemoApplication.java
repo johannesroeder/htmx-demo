9478053acac4dfcd38b2e1c6c6a31434773ecc93
One16-JR <121863164+One16-JR@users.noreply.github.com>
2024-08-06 09:17:38 +0200

    initial setup. index page working

---
+++ src/main/java/be/one16/johannesroeder/htmxdemo/HtmxDemoApplication.java
@@ -0,0 +1,13 @@
+package be.one16.johannesroeder.htmxdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HtmxDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HtmxDemoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/be/one16/johannesroeder/htmxdemo/HtmxDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package be.one16.johannesroeder.htmxdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HtmxDemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
