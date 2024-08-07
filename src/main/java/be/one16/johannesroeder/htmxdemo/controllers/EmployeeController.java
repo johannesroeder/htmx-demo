7f34c468e256d51cba27d61ad23b3d1bc843625b
One16-JR <121863164+One16-JR@users.noreply.github.com>
2024-08-07 10:00:38 +0200

    delete employee

--- src/main/java/be/one16/johannesroeder/htmxdemo/controllers/EmployeeController.java
+++ src/main/java/be/one16/johannesroeder/htmxdemo/controllers/EmployeeController.java
@@ -3,6 +3,7 @@ package be.one16.johannesroeder.htmxdemo.controllers;
 import be.one16.johannesroeder.htmxdemo.services.EmployeeService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
@@ -14,15 +15,22 @@ public class EmployeeController {
         this.employeeService = employeeService;
     }
 
-    @GetMapping("/employees")
+    @GetMapping("/employee")
     public String employees(Model model) {
         model.addAttribute("allEmpList", employeeService.getEmployees());
         return "fragments :: employeeList";
     }
 
-    @GetMapping("employee-info/{id}")
+    @GetMapping("employee/{id}")
     public String employeeInfo(@PathVariable Integer id, Model model) {
         model.addAttribute("info", employeeService.getEmployeeInfo(id));
         return "fragments :: employeeInfo";
+    }
+
+    @DeleteMapping("employee/{id}")
+    public String deleteEmployee(@PathVariable Integer id, Model model) {
+        employeeService.deleteEmployee(id);
+        model.addAttribute("allEmpList", employeeService.getEmployees());
+        return "fragments :: employeeList";
     }
 }
--- src/main/java/be/one16/johannesroeder/htmxdemo/services/EmployeeService.java
+++ src/main/java/be/one16/johannesroeder/htmxdemo/services/EmployeeService.java
@@ -4,6 +4,7 @@ import be.one16.johannesroeder.htmxdemo.domain.Employee;
 import jakarta.annotation.PostConstruct;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
 import java.util.List;
 
 @Service
@@ -12,12 +13,11 @@ public class EmployeeService {
 
     @PostConstruct
     private void init() {
-        employees = List.of(
-                new Employee(1, "Johannes", "johanne@one16.be", "Johannes is a software engineer at One16."),
-                new Employee(2, "Umberto", "umberto@one16.be", "Umberto is a software engineer at One16."),
-                new Employee(3, "Maarten", "maarten@one16.be", "Maarten is a software engineer at One16."),
-                new Employee(4, "Arno","arno@one16.be", "Arno is a software engineer at One16.")
-        );
+        employees = new ArrayList<>(4);
+        employees.add(new Employee(1, "Johannes", "johanne@one16.be", "Johannes is a software engineer at One16."));
+        employees.add(new Employee(2, "Umberto", "umberto@one16.be", "Umberto is a software engineer at One16."));
+        employees.add(new Employee(3, "Maarten", "maarten@one16.be", "Maarten is a software engineer at One16."));
+        employees.add(new Employee(4, "Arno","arno@one16.be", "Arno is a software engineer at One16."));
     }
 
     public List<Employee> getEmployees() {
@@ -30,5 +30,9 @@ public class EmployeeService {
                 .findFirst()
                 .map(Employee::info)
                 .orElse("No info found.");
+    }
+
+    public void deleteEmployee(Integer id) {
+        employees.removeIf(employee -> employee.id().equals(id));
     }
 }
